9f7f91b9a5ce5d668b7366c879fece6465a56762
AlaaAldeenBK <alshamery.alaaaldeen@gmail.com>
2022-08-03 16:45:52 +0300

    node js file

---
+++ index.js
@@ -0,0 +1,26 @@
+const event = require("events");
+const server = require("http")
+const starting = server.createServer((req,res) =>{
+    eEmit.emit("onLunch", req);
+    res.write("Inside Server");
+    res.end();
+});
+const eEmit = new event();
+
+eEmit.on("onLunch",(request) =>{
+    console.log("Event With no parameter");
+});
+
+// eEmit.on("onLunch",(x,y) =>{
+//     console.log("Event with two parameter");
+//     console.log(`${x} , ${y}`);
+// });
+
+// eEmit.on("onLunch", (request,...par)=>{
+//     console.log("Event with many parameter");
+//     console.log(`${par}`);
+// });
+
+
+
+starting.listen("3000")
